83bb344bc00f0b2c5e5149f730efbc4aa70b2ce3
AJosueBN <anthonyjosue.bn@gmail.com>
2023-07-17 21:46:33 +0000

    Added login handler

--- public/js/login.js
+++ public/js/login.js
@@ -1,2 +1,31 @@
 // Query Selectors
 
+const username = document.querySelector('#username-login').value.trim();
+const email = document.querySelector('#email-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
+
+
+const loginFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    if (username && email && password) {
+      // Send the username, e-mail and password to the server
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username , email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
