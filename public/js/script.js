976aecb533afb1ae565f67c0643408f8c98f7e1a
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-07-19 15:41:31 +0100

    logout modal added

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,14 @@ const handleLogout = async () => {
 };
 
   
+const openModal = document.querySelector('[data-open-modal]')
+const closeModal = document.querySelector('[data-close-modal]')
+const modal = document.querySelector('[data-modal]')
+
+openModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close()
+})
